28febd6b0f570220afa5f2e94781ea8066682c2e
Robin <robin-be@hotmail.be>
2021-01-06 18:13:57 +0100

    env config

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -115,7 +115,7 @@ class AppController extends AbstractController
     }
 
     /**
-     * @Route("/pay/{id}",name="pay", methods={"POST"})
+     * @Route("/pay/{id}",name="pay", methods={"POST"}, schemes={"https"})
      */
     public function paying(int $id): Response
     {
